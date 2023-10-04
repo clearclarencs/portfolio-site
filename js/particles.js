fb52888f49128ea5b647ca7ab4626ed47a0baa68
clearclarencs <53274728+clearclarencs@users.noreply.github.com>
2023-10-04 14:52:05 +0100

    Improved particle stability

--- js/particles.js
+++ js/particles.js
@@ -2,7 +2,7 @@ let skillObj;
 let fontSize = 32;
 var skills = [];
 
-const skillNames = ["Python", "Javascript", "Git", "HTML", "CSS", "SQL", "Linux", "Docker"]
+const skillNames = ["Python", "JavaScript", "Git", "HTML", "CSS", "SQL", "Linux"]
 
 function between(x, min, max) {
   return x >= min && x <= max;
@@ -15,7 +15,7 @@ function setup() {
   for (skillName in skillNames){
     x = random(width);
     y = random(height);
-    skills.push({"name":skillNames[skillName], "x":x, "y":y, "xspeed": 1, "yspeed":1})
+    skills.push({"name":skillNames[skillName], "x":x, "y":y, "xspeed": 0.5, "yspeed":0.5})
   }
 }
 
@@ -29,8 +29,8 @@ function draw() {
   textSize(fontSize);
   for (skill in skills){
     skillObj = skills[skill]["name"];
-    textH = fontSize
-    textW = textWidth(skillObj)
+    textH = fontSize + 5
+    textW = textWidth(skillObj) + 5
     text(skillObj, skills[skill]["x"], skills[skill]["y"]);
     fill(29, 161, 242, 250);
 
@@ -47,8 +47,10 @@ function draw() {
     for (skillOth in skills){
       if (skill == skillOth){continue;}
       if (between(skills[skillOth]["x"], skills[skill]["x"] - textW, skills[skill]["x"] + textW) && between(skills[skillOth]["y"], skills[skill]["y"] - textH, skills[skill]["y"] + textH)){
-        if (((skills[skill]["yspeed"] > 0) && (skills[skill]["y"] >= skills[skillOth]["y"])) || ((skills[skill]["yspeed"] < 0) && (skills[skill]["y"] <= skills[skillOth]["y"]))){continue;} // Already travelling away
-        if (((skills[skill]["xspeed"] > 0) && (skills[skill]["x"] >= skills[skillOth]["x"])) || ((skills[skill]["xspeed"] < 0) && (skills[skill]["x"] <= skills[skillOth]["x"]))){continue;} // Already travelling away
+        if ((skills[skill]["yspeed"] != skills[skillOth]["yspeed"]) && (skills[skill]["xspeed"] != skills[skillOth]["xspeed"])){ // If travelling diff directions
+          if (((skills[skill]["yspeed"] > 0) && (skills[skill]["y"] >= skills[skillOth]["y"])) && ((skills[skill]["yspeed"] < 0) && (skills[skill]["y"] <= skills[skillOth]["y"]))){continue;} // Already travelling away
+          if (((skills[skill]["xspeed"] > 0) && (skills[skill]["x"] >= skills[skillOth]["x"])) && ((skills[skill]["xspeed"] < 0) && (skills[skill]["x"] <= skills[skillOth]["x"]))){continue;} // Already travelling away
+        }
         skills[skill]["yspeed"] = -skills[skill]["yspeed"];
         skills[skill]["xspeed"] = -skills[skill]["xspeed"];
         skills[skillOth]["yspeed"] = -skills[skillOth]["yspeed"];
